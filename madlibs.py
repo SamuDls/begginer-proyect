378342fb272be3b3a4204e2c3ed082c1d096f2f5
Samkode <samu92dls@gmail.com>
2023-03-30 06:33:29 -0300

    madlibs ended

---
+++ madlibs.py
@@ -0,0 +1,25 @@
+adjetive = input("Escribe un adjetivo: ")
+adjective2 = input("Esribe un adjetivo2: ")
+noun = input("Esribe un sustantivo: ")
+noun2 = input("Esribe un sustantivo2: ")
+plural_noun = input("Esribe un sustantivo plural: ")
+game = input("Esribe un juego: ")
+plural_noun2 = input("Esribe un sustantivo plural2: ")
+ing_verb = input("Esribe un verbo en presente continuo: ")
+ing_verb2 = input("Esribe un verbo en presente continuo2: ")
+plural_noun3 = input("Esribe un sustantivo plural3: ")
+ing_verb3 = input("Esribe un verbo en presente continuo3: ")
+noun3 = input("Esribe un sustantivo3: ")
+plant = input("Esribe un nombre de planta: ")
+body_part = input("Esribe un nombre de parte del cuerpo: ")
+place = input("Esribe un nombre de lugar: ")
+ing_verb4 = input("Esribe un verbo en presente continuo4: ")
+adjective3 = input("Esribe un adjetivo3: ")
+number = input("Esribe un numero: ")
+plural_noun4 = input("Esribe un sustantivo plural4: ")
+
+
+madlibs = f"A vacation is when you take a trip to some {adjetive}  place with your {adjective2} family. Usually you go to some place that is near a/an {noun} or up on a/an {noun2}. A good vacation place is one where you can ride {plural_noun} or play {game} or go hunting for {plural_noun2} . I like to spend my time {ing_verb}  or {ing_verb2}. When parents go on a vacation, they spend their time eating three {plural_noun3}  a day, and fathers play golf, and mothers sit around {ing_verb3} . Last summer, my little brother  fell in a/an {noun3}  and got poison {plant}  all over his {body_part} . My family is going to go to the {place} , and I will practice {ing_verb4} . Parents need vacations more than kids because parents are always very  {adjective3}  and because they have to work {number} hours every day all year making enough {plural_noun4} to pay for the vacation."
+
+
+print(madlibs)
