552f301add9c3b0c971e31a057c96783a4d2cf05
Samkode <samu92dls@gmail.com>
2023-04-11 05:26:47 -0300

    start guess_number project

---
+++ guess_number.py
@@ -0,0 +1,12 @@
+import random
+
+
+def run():
+   print("=============================================================================")
+   print("""Bienvenido a el juego adivina el numero,
+debes ingresar un numero entero para intentar adivinar, Buena suerte!""")
+   print("=============================================================================")
+
+
+if __name__== "__main__":
+   run()
